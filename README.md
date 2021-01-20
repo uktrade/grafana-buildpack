bb51aa8f78e87f767f58af68c09a7591a1c52521
dvadgama <dhaval.it@gmail.com>
2021-01-20 05:23:58 +0000

    Grafana buildpack - added detect script which accepts all buildpacks - added envrc as a helper script to set variables - added supply script to download elected version of grafana - added finalize script to install grafana and custom files - added env.sh to setup running configuration - added release script to set default run command

---
+++ README.md
@@ -0,0 +1,53 @@
+# **Grafana Buildpack**
+
+This build pack deploys the specified version of Grana to your cloud foundry and, it is designed to work with GDS PaaS 
+
+## **Features**
+- It can install user specified version of grafana
+- It can automatically select between sqllite3 or compitable database ( Postgres ) to bind or It connect to user specified database 
+
+## **Limitations**
+- supports only Postgres and Sqlite3
+
+## **Note**
+Use postgres db if you would like to preserve your configuration and , dashboards between deployments 
+
+## **Example manifest.yml**
+
+```yaml
+---
+applications:
+  - buildpacks:
+      - https://github.com/uktrade/grafana-buildpack.git
+```
+
+cf v2 command
+```bash
+cf push 
+```
+
+cf v3 command
+```bash
+cf v3-push -b https://github.com/uktrade/grafana-buildpack.git
+```
+
+
+## **Configu variables**
+
+| Variable                | Default Value |
+|:------------------------|:--------------|
+| $APP_MODE               | production    |
+| $ADMIN_USER             | admin         |
+| $ADMIN_PASSWORD         | admin         |
+| $SECRET_KEY             | space guid    |
+| $AUTO_DB_SETUP          | false         |
+| $GRAFANA_DB_SERVICE_NAME| null          |
+
+## **Custom Config Files**
+- **runtime.txt**: User can specify the version of Grafana that needs to be installed , current default version is 6.3.5
+
+- **grafana.ini**: With this file , user can override the default configuration
+
+## Todo
+- Validate downloaded file with checksum
+- make it work with mysql, currently Grafana seems to have trouble with useSSL=True in mysql URI and, suggested work around is to use ClOUD SQL PROXY , which is something needs to be done if need arises
---
+++ deps/env.sh
@@ -0,0 +1,86 @@
+#!/user/bin/env bash
+export_app_env(){
+
+export APP_ROOT="${HOME}"
+export APP_MODE="${APP_MODE:-production}"
+export APP_DATA_DIR="${APP_ROOT}/data"
+export APP_PLUGINS_DIR="${APP_DATA_DIR}/plugins"
+export APP_LOGS_DIR="${APP_DATA_DIR}/log"
+
+export APP_PROVISIONING_DIR="${APP_ROOT}/conf/provisioning"
+
+export DOMAIN=${DOMAIN:-$(jq -r '.uris[0]' <<<"${VCAP_APPLICATION}")}
+export URL="${URL:-https://${DOMAIN}}"
+
+export ADMIN_USER="${ADMIN_USER:-admin}"
+export ADMIN_PASS="${ADMIN_PASS:-admin}"
+export SECRET_KEY=${SECRET_KEY:-$(jq -r '.space_id' <<<"${VCAP_APPLICATION}")}
+export APP_LOG_MODE="${APP_LOG_MODE:-console}"
+
+}
+
+export_db_env(){
+
+    export AUTO_DB_SETUP="${AUTO_DB_SETUP:-fale}"
+    export GRAFANA_DB_SERVICE_NAME="${GRAFANA_DB_SERVICE_NAME:-null}"
+
+    local db=''
+
+    if [ "${GRAFANA_DB_SERVICE_NAME}" != null ] 
+    then 
+        export AUTO_DB_SETUP=false
+        db=$(jq -r --arg instance_name $GRAFANA_DB_SERVICE_NAME '.[][] | select(.instance_name == $instance_name)' <<<  "${VCAP_SERVICES}")
+        #if DB is not found , let user know 
+        if [ -z "${db}" ]
+        then
+            echo "Service ${GRAFANA_DB_SERVICE_NAME} not found"
+        fi
+    fi
+
+    if [[ "${AUTO_DB_SETUP}" == true || "${AUTO_DB_SETUP}" == True || "${AUTO_DB_SETUP}" == TRUE || "${AUTO_DB_SETUP}" == 1 ]] 
+    then
+        db=$(jq -r '.[][] | select(.label == ("postgres","mysql"))' <<<  "${VCAP_SERVICES}")
+        #if DB is not found , let user know 
+        if [ -z "${db}" ]
+        then
+            echo "AUTO_DB_SETUP did not find any supported databases"
+        fi
+    fi
+
+    local dbtype=$(jq -r '.label' <<< "${db}")
+    local dbname=$(jq -r '.credentials.database' <<< "${db}")
+    local dbuser=$(jq -r '.credentials.username' <<< "${db}")
+    local dbpass=$(jq -r '.credentials.password' <<< "${db}")
+    local dbhost=$(jq -r '.credentials.hostname' <<< "${db}")
+    local dbport=$(jq -r '.credentials.port' <<< "${db}")
+    local dburi=$(jq -r '.credentials.uri' <<< "${db}")
+
+    export DB_TYPE="${dbtype:-sqlite3}"
+    export DB_NAME="${dbname:-grafana}"
+    export DB_USER="${dbuser:-root}"
+    export DB_PASSWORD="${dbpass:-}"
+    export DB_HOSTNAME="${dbhost:-}"
+    export DB_PORT="${dbport:-}"
+    export DB_URI="${dburi:-${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}}"
+
+    export DB_SSL_MODE=""
+    export CA_CERT_PATH=""
+    
+    if [[ ${DB_TYPE} == "postgres" ]]
+    then
+        export DB_SSL_MODE="require"
+    fi
+    
+    if [[ ${DB_TYPE} == "mysql" ]]
+    then
+        export DB_SSL_MODE="skip-verify"
+        export CA_CERT_PATH="/etc/ssl/certs/ca-certificates.crt"
+    fi
+}
+
+main(){
+    export_app_env
+    export_db_env
+}
+
+main
